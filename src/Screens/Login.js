3f4fea1270ec7d1567061926315c409d5b112733
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2020-01-05 22:29:37 +0100

    login ui tweaks

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+
+
 class Login extends Component{
     render(){
         return(
-            <div>
-                <h1>User</h1>
-                <h1>Pass</h1>
-                <button onClick={() => this.props.onLoginClicked()}> Login</button>
+            <div style={{backgroundColor:'#001f3f'}}>
+                <h1>Username</h1>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <h1>Password</h1>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <Button onClick={() => this.props.onLoginClicked()} variant="outlined">Login</Button>
             </div>
         )
     }
